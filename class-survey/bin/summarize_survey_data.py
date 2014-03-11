7e0e7d5af31fff34293be0a386f21ee472bcbb31
Jamie Oaks <joaks1@gmail.com>
2014-03-11 01:08:48 -0700

    Updating script for summarizing class-survey data.

--- class-survey/bin/summarize_survey_data.py
+++ class-survey/bin/summarize_survey_data.py
@@ -2,8 +2,10 @@
 
 import os
 import sys
+import scipy.stats
 
 from pymsbayes import plotting
+from pymsbayes.utils import stats
 
 class StudentResult(object):
     valid_answers = [0, 1]
@@ -12,6 +14,8 @@ class StudentResult(object):
         self.answers = answers
         self.graduate = graduate
         self.year = year
+        assert ((self.number_correct + self.number_wrong) ==
+                self.number_of_questions)
 
     def _set_answers(self, answers):
         self._answers = [int(a) for a in answers]
@@ -127,7 +131,12 @@ class TestResult(object):
 
     def overall_result_iter(self):
         for r in self.results:
-            yield r.number_correct
+            yield r.number_correct, r.number_wrong
+
+    def overall_proportion_iter(self):
+        for nc, nw in self.overall_result_iter():
+            t = float(nc + nw)
+            yield nc / t, nw /t
 
 
 class ClassResult(object):
@@ -142,22 +151,27 @@ class ClassResult(object):
         self.number_of_questions = self.after_result.number_of_questions
         self.question_offset = question_offset
 
-    def get_question_plot(self, question_index):
-        bpc, bpw = self.before_result.get_question_proportions(question_index)
-        apc, apw = self.after_result.get_question_proportions(question_index)
-        dc = apc - bpc
-        plot_text = r'$\Delta correct = {0}$'.format(dc)
+    def _get_plot(self, bnc, bnw, anc, anw,
+            title = ''):
+        btotal = float(bnc + bnw)
+        atotal = float(anc + anw)
+        bpc, bpw = bnc / btotal, bnw / btotal
+        apc, apw = anc / atotal, anw / atotal
+        odds_ratio, pval = scipy.stats.fisher_exact([[anc, anw], [bnc, bnw]],
+                # alternative = 'greater')
+                alternative = 'two-sided')
+        plot_text = r'$p = {0:.3f}$'.format(pval)
         sbd = plotting.StackedBarData(
-                top_values = [bpc, apc],
-                bottom_values = [bpw, apw],
-                labels = ['before', 'after'],
+                top_values = [bpw, apw],
+                bottom_values = [bpc, apc],
+                labels = ['Before', 'After'],
                 width = 0.5,
                 orientation = 'vertical',
                 top_color = '0.75',
                 bottom_color = '0.35',
-                top_label = 'correct',
-                bottom_label = 'incorrect',
-                label_size = 12.0,
+                top_label = 'Incorrect',
+                bottom_label = 'Correct',
+                label_size = 16.0,
                 measure_tick_label_size = 10.0,
                 extra_plot_space = 0.6)
         sp = plotting.ScatterPlot(
@@ -165,32 +179,117 @@ class ClassResult(object):
                 plot_label = None,
                 x_label = None,
                 y_label = None,
-                y_label_size = 12.0)
-                # right_text = plot_text)
+                y_label_size = 12.0,
+                right_text = plot_text)
         pg = plotting.PlotGrid(
             subplots = [sp],
             num_columns = 1,
             label_schema = None,
-            title = 'Question {0}'.format(question_index + self.question_offset),
+            title = title,
             title_top = True,
-            title_size = 14.0,
+            title_size = 18.0,
             y_title = 'Proportion',
             y_title_position = 0.001,
-            y_title_size = 12.0,
+            y_title_size = 14.0,
             height = 4.0,
             width = 6.0,
             auto_height = False)
         pg.margin_left = 0.04
         pg.margin_right = 1
         pg.margin_bottom = 0
-        pg.margin_top = 0.95
+        pg.margin_top = 0.935
         pg.auto_adjust_margins = False
         pg.reset_figure()
+        return pg
+
+    def get_overall_plot(self):
+        bnc, bnw = 0, 0
+        anc, anw = 0, 0
+        for nc, nw in self.before_result.overall_result_iter():
+            bnc += nc
+            bnw += nw
+        for nc, nw in self.after_result.overall_result_iter():
+            anc += nc
+            anw += nw
+        pg = self._get_plot(bnc = bnc,
+                bnw = bnw,
+                anc = anc,
+                anw = anw,
+                title = 'All questions')
+        return pg
+
+    def get_question_plot(self, question_index):
+        bnc, bnw = self.before_result.get_question_results(question_index)
+        anc, anw = self.after_result.get_question_results(question_index)
+        title = 'Question {0}'.format(question_index + self.question_offset)
+        pg = self._get_plot(bnc = bnc,
+                bnw = bnw,
+                anc = anc,
+                anw = anw,
+                title = title)
         return pg
 
     def question_plot_iter(self):
         for q in range(self.number_of_questions):
             yield self.get_question_plot(q)
+
+    def get_student_histograms(self, title = 'All questions'):
+        b_results = [pc for (pc, pw) in self.before_result.overall_proportion_iter()]
+        a_results = [pc for (pc, pw) in self.after_result.overall_proportion_iter()]
+        b_sum = stats.get_summary(b_results)
+        a_sum = stats.get_summary(a_results)
+        b_text = r'$mean = {mean:.3f}$; $median = {median:.3f}$'.format(**b_sum)
+        a_text = r'$mean = {mean:.3f}$; $median = {median:.3f}$'.format(**a_sum)
+        bins = [0.0, 1/6.0, 2/6.0, 3/6.0, 4/6.0, 5/6.0, 1.0]
+        b_hist_data = plotting.HistData(b_results,
+                normed = True,
+                align = 'mid',
+                bins = bins)
+        a_hist_data = plotting.HistData(a_results,
+                normed = True,
+                align = 'mid',
+                bins = bins)
+        b_hist = plotting.ScatterPlot(
+                hist_data_list = [b_hist_data],
+                plot_label = None,
+                x_label = None,
+                y_label = None,
+                y_label_size = 12.0,
+                right_text = b_text)
+        a_hist = plotting.ScatterPlot(
+                hist_data_list = [a_hist_data],
+                plot_label = None,
+                x_label = None,
+                y_label = None,
+                y_label_size = 12.0,
+                right_text = a_text)
+        pg = plotting.PlotGrid(
+            subplots = [b_hist, a_hist],
+            num_columns = 1,
+            label_schema = None,
+            share_x = True,
+            title = 'Student scores',
+            title_top = False,
+            title_size = 14.0,
+            y_title = 'Density',
+            y_title_position = 0.001,
+            y_title_size = 14.0,
+            super_title = title,
+            super_title_size = 18.0,
+            row_labels = ['Before', 'After'],
+            row_label_size = 18.0,
+            row_label_offset = 0.03,
+            height = 7.0,
+            width = 6.0,
+            auto_height = False)
+        pg.margin_left = 0.04
+        pg.margin_right = 0.95
+        pg.margin_bottom = 0.03
+        pg.margin_top = 0.94
+        pg.auto_adjust_margins = False
+        pg.reset_figure()
+        return pg
+
 
 def main():
     import project_util
@@ -209,6 +308,14 @@ def main():
             out_path = os.path.join(pdir,
                     'question-{0}.pdf'.format(i + results.question_offset))
             p.savefig(out_path)
+        p = results.get_overall_plot()
+        out_path = os.path.join(pdir,
+                'all-questions.pdf')
+        p.savefig(out_path)
+        p = results.get_student_histograms()
+        out_path = os.path.join(pdir,
+                'student-hist.pdf')
+        p.savefig(out_path)
 
 if __name__ == '__main__':
     main()
